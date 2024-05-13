f8690c6be83d2ba7c6c4eb0743e968b40aa0f0c8
Naufal0067 <naufalopang1928@gmail.com>
2024-05-13 14:40:33 +0700

    class mahasiswa

--- mbrStatik.cpp
+++ mbrStatik.cpp
@@ -1,2 +1,18 @@
 #include <iostream>
 using namespace std;
+
+class mahasiswa
+{
+public:
+    static int nimm;
+    int id;
+    string nama;
+
+    void setId();
+
+    void printAll();
+    mahasiswa(string pnama):nama(pnama)
+    {
+        setID;
+    }
+};

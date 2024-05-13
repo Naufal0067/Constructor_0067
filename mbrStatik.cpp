2280ef053a60c1caaeb9446bf4ff3c8d5fa0b934
Naufal0067 <naufalopang1928@gmail.com>
2024-05-13 14:47:43 +0700

    void mahasiswa set id dan void mahasiswa printall

--- mbrStatik.cpp
+++ mbrStatik.cpp
@@ -18,3 +18,16 @@ public:
 };
 
 int mahasiswa::nim = 20;
+
+void mahasiswa::setID()
+{
+    id = nim++;
+}
+
+void mahasiswa::printAll()
+{
+    cout << "ID  = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+}
+
